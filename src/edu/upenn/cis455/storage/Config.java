57d08dbb72bc5db9978808ed9f7880cb500308c9
momocoisapeach <peach@hntvlan569.1924.wlan.wireless-resnet.upenn.edu>
2015-04-26 15:01:46 -0400

    machine=0

--- src/edu/upenn/cis455/storage/Config.java
+++ src/edu/upenn/cis455/storage/Config.java
@@ -9,12 +9,12 @@ package edu.upenn.cis455.storage;
  */
 public class Config {
 
-	public static int machine = 1;
-	public static String[] DocID_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db38/id.txt", "/Users/dichenli/Downloads/tryCrawl/id.txt"};
+	public static int machine = 0;
+	public static String[] DocID_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db39/id.txt", "/Users/dichenli/Downloads/tryCrawl/id.txt"};
 
-	public static String[] DocLinks_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db38/links.txt", "/Users/dichenli/Downloads/tryCrawl/links.txt"};
+	public static String[] DocLinks_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db39/links.txt", "/Users/dichenli/Downloads/tryCrawl/links.txt"};
 
-	public static String[] DocContent_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db38/content/", "/Users/dichenli/Downloads/tryCrawl/content/"};
+	public static String[] DocContent_Files = {"/Users/peach/Documents/upenn/2015spring/cis555/db/db39/content/", "/Users/dichenli/Downloads/tryCrawl/content/"};
 
 	public static String DocID_File = DocID_Files[machine];
 	public static String DocLinks_File = DocLinks_Files[machine];
