710df9cc5cc14d953c724e9244382163704cde27
dichenli <dichenli@sas.upenn.edu>
2015-04-29 20:16:35 -0400

    ad jar

--- src/Script/Script.java
+++ src/Script/Script.java
@@ -13,7 +13,7 @@ import Utils.IOUtils;
  * @author dichenli
  *
  */
-public class Script {
+public class Script extends Thread {
 
 	static String[] addresses = {
 		"ec2-user@52.24.15.232", //0
@@ -68,14 +68,100 @@ public class Script {
 	static String remote_home = "/home/ec2-user/";
 	static String crawl_data = remote_home + "crawl_data/";
 	static long crawl_limit = 10000;
-	static double size = 1; //Mb data
+	static int size = 1; //Mb data
 	static int crawler_num = 18;
 
+	int i; //crawler number
+	
+	@Override
+	public void run() {
+		System.out.println("Thread start: " + i);
+		boolean success = true;
+		PrintWriter writer = null;
+		String filenameEc2Script = ec2_home + "ec2_script" + i;
+		File ec2Script = new File(filenameEc2Script);
+		success &= IOUtils.createFile(ec2Script);
+		if(!success) {
+			System.err.println("create file ec2 failed");
+			return;
+		}
+		try {
+			writer = IOUtils.getWriter(ec2Script);
+			writer.println("unzip " + upload_zip);
+			writer.println("mkdir ~/.aws");
+			writer.println("mv " + remote_home + "credentials ~/.aws/credentials"); 
+			writer.close();
+			IOUtils.setFilePermission(ec2Script, 7, 0, 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		String filenameEC2_run = ec2_home + "run_crawl" + i;
+		File ec2_run = new File(filenameEC2_run);
+		success &= IOUtils.createFile(ec2_run);
+		if(!success) {
+			System.err.println("create file ec2 failed");
+			return;
+		}
+		try {
+			writer = IOUtils.getWriter(ec2_run);
+			writer.println("java -jar " + crawl_data + "Crawler.jar " + seeds[i] + " " + crawl_data + " " + size + " " + crawl_limit + " " + i + " " + crawler_num);
+			writer.close();
+			IOUtils.setFilePermission(ec2_run, 7, 0, 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		String filenameUpload = ec2_home + "upload_crawler" + i;
+		File uploadScript = new File(filenameUpload);
+
+		success &= IOUtils.createFile(uploadScript);
+		if(!success) {
+			System.err.println("create file crawler failed");
+			return;
+		}
+
+		try {
+			writer = IOUtils.getWriter(uploadScript);
+			writer.println("scp -i " + key_pem + " " + local_download + upload_zip + " " + addresses[i] + ":~/");
+			writer.println("scp -i " + key_pem + " " + aws_home + "credentials " + addresses[i] + ":~/credentials");
+			writer.println("scp -i " + key_pem + " " + filenameEc2Script + " " + addresses[i] + ":~/");
+			writer.println("scp -i " + key_pem + " " + filenameEC2_run + " " + addresses[i] + ":~/");
+			writer.close();
+			IOUtils.setFilePermission(uploadScript, 7, 0, 0);
+			System.out.println("upload for crawler: " + i);
+			boolean rv = IOUtils.runtimeExec(uploadScript.getAbsolutePath());
+			if(!rv) {
+				System.err.println("upload error");
+			}
+			System.out.println("upload for crawler done");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		String filenameSSH = ec2_home + "crawler" + i;
+		File ssh = new File(filenameSSH);
+		try {
+			IOUtils.createFile(ssh);
+			writer = IOUtils.getWriter(ssh);
+			writer.println("ssh -i " + "ec2try.pem" + " " + addresses[i]);
+			writer.close();
+			IOUtils.setFilePermission(ssh, 7, 0, 0); 
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ;
+		}
+	}
+	
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		if(addresses.length != crawler_num || seeds.length != crawler_num) {
 			System.err.println("wrong seeds or addresses!");
 			return;
@@ -93,82 +179,14 @@ public class Script {
 		IOUtils.runtimeExec(zip.getAbsolutePath());
 
 //		PrintWriter writer = null;
+		Script[] scripts = new Script[crawler_num];
 		for (int i = 0; i < crawler_num; i++) {
-			
-			String filenameEc2Script = ec2_home + "ec2_script" + i;
-			File ec2Script = new File(filenameEc2Script);
-			success &= IOUtils.createFile(ec2Script);
-			if(!success) {
-				System.err.println("create file ec2 failed");
-				return;
-			}
-			try {
-				writer = IOUtils.getWriter(ec2Script);
-				writer.println("unzip " + upload_zip);
-				writer.println("mkdir ~/.aws");
-				writer.println("mv " + remote_home + "credentials ~/.aws/credentials"); 
-				writer.close();
-				IOUtils.setFilePermission(ec2Script, 7, 0, 0);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw e;
-			}
-			
-			String filenameEC2_run = ec2_home + "run_crawl" + i;
-			File ec2_run = new File(filenameEC2_run);
-			success &= IOUtils.createFile(ec2_run);
-			if(!success) {
-				System.err.println("create file ec2 failed");
-				return;
-			}
-			try {
-				writer = IOUtils.getWriter(ec2_run);
-				writer.println("java -jar " + crawl_data + "Crawler.jar " + seeds[i] + " " + crawl_data + " " + size + " " + crawl_limit + " " + i + " " + crawler_num);
-				writer.close();
-				IOUtils.setFilePermission(ec2_run, 7, 0, 0);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw e;
-			}
-			
-			String filenameUpload = ec2_home + "upload_crawler" + i;
-			File uploadScript = new File(filenameUpload);
-
-			success &= IOUtils.createFile(uploadScript);
-			if(!success) {
-				System.err.println("create file crawler failed");
-				return;
-			}
-
-			try {
-				writer = IOUtils.getWriter(uploadScript);
-				writer.println("scp -i " + key_pem + " " + local_download + upload_zip + " " + addresses[i] + ":~/");
-				writer.println("scp -i " + key_pem + " " + aws_home + "credentials " + addresses[i] + ":~/credentials");
-				writer.println("scp -i " + key_pem + " " + filenameEc2Script + " " + addresses[i] + ":~/");
-				writer.println("scp -i " + key_pem + " " + filenameEC2_run + " " + addresses[i] + ":~/");
-				writer.close();
-				IOUtils.setFilePermission(uploadScript, 7, 0, 0);
-				boolean rv = IOUtils.runtimeExec(uploadScript.getAbsolutePath());
-				if(!rv) {
-					System.err.println("upload error");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw e;
-			}
-			
-			String filenameSSH = ec2_home + "crawler" + i;
-			File ssh = new File(filenameSSH);
-			try {
-				IOUtils.createFile(ssh);
-				writer = IOUtils.getWriter(ssh);
-				writer.println("ssh -i " + key_pem + " " + addresses[i]);
-				writer.close();
-				IOUtils.setFilePermission(ssh, 7, 0, 0); 
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw e;
-			}
+			scripts[i] = new Script();
+			scripts[i].i = i;
+			scripts[i].start();
+		}
+		for(Script sc : scripts) {
+			sc.join();
 		}
 		
 		System.out.println("====done====");
