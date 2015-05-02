5215ae6252e044eae7e08b743e58a20f6ce06332
dichenli <dichenli@sas.upenn.edu>
2015-05-02 01:18:48 -0400

    mergeline

---
+++ src/Utils/BinaryUtilsTest.java
@@ -0,0 +1,24 @@
+package Utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryUtilsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testArrayEquals() {
+		byte[] arr1 = {0, 1, 2};
+		byte[] arr2 = {0, 1, 2};
+		byte[] arr3 = {0, 1};
+		byte[] arr4 = {0, 1, 3};
+		byte[] arr5 = null;
+		//TODO
+	}
+
+}
